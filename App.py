8746b1f23048c45d1bc9eaf2ecac554c7cf6204c
ezequielhenrique <ezequielhenrique806@gmail.com>
2024-08-31 15:01:53 -0300

    alteracao fastapi

--- App.py
+++ App.py
@@ -10,126 +10,21 @@
 #localHost/Distribuir/Exercitos(Get)
 #localHost/Distribuir/Exercitos/Distribuir(Post)
 #Quais recursos : Jogo War
-from flask import Flask, jsonify, request
-App = Flask(__name__)
 
-Cores = [
-    {
-        'id':1,
-        'Cor': "Azul",
-    },
-    {
-        'id':2,
-        'Cor': "Branca",
-        
-    },
+from fastapi import FastAPI
+from utils import cores, objetivos
 
-    {
-        'id':3,
-        'Cor': "Vermelha",
-       
-        
-    },
+app = FastAPI()
 
-    {
-        'id':4,
-        'Cor': "Preta",
-        
-        
-    },
-    {
-        'id':5,
-        'Cor': "Amarela",
-       
-        
-    },
 
-    {
-        'id':6,
-        'Cor': "Verde",
-        
-    },
+@app.get("/")
+async def root():
+    return {"Mensagem": "Bem-vindo ao jogo war"}
 
-]
 
-Objetivos=[ 
-    {
-        'id':1,
-        'Objetivos':'Conquistar na totalidade a EUROPA, a OCEANIA e mais um terceiro.'
-    },
-
-    {
-        'id':2,
-         'Objetivos':' Conquistar na totalidade a ÁSIA e a AMÉRICA DO SUL.'
-    },
-
-    {
-        'id':3,
-        'Objetivos' :' Conquistar na totalidade a EUROPA, a AMÉRICA DO SUL e mais um terceiro.'
-    },
-
-    {
-        'id':4,
-        'Objetivos':'Conquistar 18 TERRITÓRIOS e ocupar cada um deles com pelo menos dois exércitos.'
-    },
-
-    {
-        'id':5,
-       'Objetivos' :'Conquistar na totalidade a ÁSIA e a ÁFRICA.'
-    },
-
-    {
-        'id':6,
-        'Objetivos':' Conquistar na totalidade a AMÉRICA DO NORTE e a ÁFRICA.'
-    },
-
-    {
-        'id':7,
-        'Objetivos':'Conquistar 24 TERRITÓRIOS à sua escolha.'
-    },
-
-    {
-        'id':8,
-        'Objetivos':'Conquistar na totalidade a AMÉRICA DO NORTE e a OCEANIA.'
-    },
-
-    {
-        'id':9,
-        'Objetivos':'Destruir totalmente OS EXÉRCITOS AZUIS.'
-    },
-
-    {
-        'id':10,
-        'Objetivos':' Destruir totalmente OS EXÉRCITOS AMARELOS.'
-    }, 
-
-    {
-        'id':11,
-        'Objetivos':'Destruir totalmente OS EXÉRCITOS VERMELHOS.'
-    },
-
-    {
-        'id':12,
-        'Objetivos':' Destruir totalmente OS EXÉRCITOS PRETOS.'
-    },
-
-    {
-        'id':13,
-        'Objetivos':'Destruir totalmente OS EXÉRCITOS BRANCO.'
-    },
-
-    {
-        'id':14,
-        'Objetivos':'Destruir totalmente OS EXÉRCITOS VERDES'
-    }
-
-]
-
-#Consultar Cor
-@App.route('/Cores',methods=['GET']) 
-def obter_Cor():
-    print("Digite a cor que deseja usar : ")
-    return jsonify(Cores)  
-App.run(port=5000, host='localhost', debug=True)
-
-#Consultar
+@app.get("/escolher-cor/{cor}")
+async def escolher_cor(cor):
+    return {
+                "Mensagem": "Você escolheu a cor", 
+                "Cor": cores[int(cor)],
+            }
---
+++ utils.py
@@ -0,0 +1,85 @@
+cores = [
+    {
+        'id':1,
+        'Cor': "Azul",
+    },
+    {
+        'id':2,
+        'Cor': "Branca",
+    },
+    {
+        'id':3,
+        'Cor': "Vermelha",
+    },
+    {
+        'id':4,
+        'Cor': "Preta",
+    },
+    {
+        'id':5,
+        'Cor': "Amarela",
+    },
+    {
+        'id':6,
+        'Cor': "Verde",
+    },
+]
+
+objetivos=[ 
+    {
+        'id':1,
+        'Objetivos':'Conquistar na totalidade a EUROPA, a OCEANIA e mais um terceiro.'
+    },
+    {
+        'id':2,
+         'Objetivos':' Conquistar na totalidade a ÁSIA e a AMÉRICA DO SUL.'
+    },
+    {
+        'id':3,
+        'Objetivos' :' Conquistar na totalidade a EUROPA, a AMÉRICA DO SUL e mais um terceiro.'
+    },
+    {
+        'id':4,
+        'Objetivos':'Conquistar 18 TERRITÓRIOS e ocupar cada um deles com pelo menos dois exércitos.'
+    },
+    {
+        'id':5,
+       'Objetivos' :'Conquistar na totalidade a ÁSIA e a ÁFRICA.'
+    },
+    {
+        'id':6,
+        'Objetivos':' Conquistar na totalidade a AMÉRICA DO NORTE e a ÁFRICA.'
+    },
+    {
+        'id':7,
+        'Objetivos':'Conquistar 24 TERRITÓRIOS à sua escolha.'
+    },
+    {
+        'id':8,
+        'Objetivos':'Conquistar na totalidade a AMÉRICA DO NORTE e a OCEANIA.'
+    },
+    {
+        'id':9,
+        'Objetivos':'Destruir totalmente OS EXÉRCITOS AZUIS.'
+    },
+    {
+        'id':10,
+        'Objetivos':' Destruir totalmente OS EXÉRCITOS AMARELOS.'
+    }, 
+    {
+        'id':11,
+        'Objetivos':'Destruir totalmente OS EXÉRCITOS VERMELHOS.'
+    },
+    {
+        'id':12,
+        'Objetivos':' Destruir totalmente OS EXÉRCITOS PRETOS.'
+    },
+    {
+        'id':13,
+        'Objetivos':'Destruir totalmente OS EXÉRCITOS BRANCO.'
+    },
+    {
+        'id':14,
+        'Objetivos':'Destruir totalmente OS EXÉRCITOS VERDES'
+    }
+]
