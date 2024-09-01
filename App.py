16df23ca5fc5961af50cd61a9ca706834ddf63f4
ezequielhenrique <ezequielhenrique806@gmail.com>
2024-08-31 21:37:51 -0300

    rota da ordem e territorios adcionadas

--- App.py
+++ App.py
@@ -12,7 +12,7 @@
 #Quais recursos : Jogo War
 
 from fastapi import FastAPI
-from utils import cores, objetivos
+from utils import cores, objetivos, territorios
 import random
 
 app = FastAPI()
@@ -20,24 +20,46 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"Mensagem": "Bem-vindo ao jogo war, Você precisa escolher a cor do Exercito",
-            "Cor do Exército" : "1-Azul, 2-Branca, 3-Vermelha, 4-Preta, 5-Amarela, 6-Verde",
-            "Mensagem ": "Para seguir o jogo altere a rota para /escolher-cor/numero da sua cor"}
+    return {
+        "Titulo": "Bem-vindo ao jogo war, Você precisa escolher a cor do Exercito",
+        "Cor do Exército" : "1-Azul, 2-Branca, 3-Vermelha, 4-Preta, 5-Amarela, 6-Verde",
+        "Mensagem ": "Para seguir o jogo altere a rota para /escolher-cor/numero da sua cor"
+    }
 
 
 @app.get("/escolher-cor/{cor}")
 async def escolher_cor(cor):
     return {
-                "Mensagem": "Você escolheu a cor", 
-                "Cor": cores[int(cor)],
-                "Mensagem ": "Após a cor desejada selecionada mude a rota para ReceberObjetivo e receba o obejtivo no jogo"
-            }
+        "Titulo": "Você escolheu a cor", 
+        "Cor": cores[int(cor)-1],
+        "Mensagem ": "Após a cor desejada selecionada mude a rota para ReceberObjetivo e receba o obejtivo no jogo"
+    }
+
 
 @app.get("/ReceberObjetivo/")
 async def ReceberObjetivo():
     objetivo=random.choice(objetivos)
     return {
-                "Mensagem": "Você recebeu o objetivo", 
-                "Objetivo":  objetivo["Objetivos"]
-            }
+        "Titulo": "Você recebeu o objetivo", 
+        "Objetivo":  objetivo["Objetivos"],
+        "Mensagem": "Para saber qual sua vez de jogar acesse a rota ordem-jogadores",
+    }
 
+
+@app.get("/ordem-jogadores/")
+async def definir_ordem():
+    ordem = random.randint(1, 6)
+    return {
+        "Titulo": f"Você será o {ordem}° jogador a jogar",
+        "Ordem": ordem,
+        "Mensagem": "Para saber seu território inicial acesse a rota meus-territorios",
+    }
+
+
+@app.get("/meus-territorios")
+async def meus_territorios():
+    territorio = random.choice(territorios)
+    return {
+        "Titulo": f'Você recebeu o território',
+        "Territorio": territorio['Território'],
+    }
